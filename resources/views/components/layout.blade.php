134fee582804fbd84eab8ce4569187b5240b6209
zielww <118595018+zielww@users.noreply.github.com>
2025-01-01 23:00:37 +0800

    Initialize Bite Notes

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+</head>
+<body>
+<nav>
+    <x-nav-link href="/">Home</x-nav-link>
+    <x-nav-link href="/about">About</x-nav-link>
+    <x-nav-link href="/contact">Contact</x-nav-link>
+</nav>
+    {{ $slot }}
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello Lola from Contact!</h1>
+</x-layout>
